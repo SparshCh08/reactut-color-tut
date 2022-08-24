28600bae9136ca2dc587bdf3280b631b639167e2
SparshCh08 <52466156+SparshCh08@users.noreply.github.com>
2022-08-24 23:14:47 +0530

    update

---
+++ src/Input.js
@@ -0,0 +1,28 @@
+import colorNames from 'colornames'
+
+const Input = ({ colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText }) => {
+    return (
+        <form onSubmit={(e) => e.preventDefault()}>
+            <input
+                autoFocus
+                type="text"
+                placeholder="Add Color Name"
+                required
+                value={colorValue}
+                onChange={(e) => {
+                    setColorValue(e.target.value);
+                    setHexValue(colorNames(e.target.value));
+                }
+                }
+            ></input>
+            <button
+                type='button'
+                onClick={() => setIsDarkText(!isDarkText)}
+            >
+                Toggle
+            </button>
+        </form>
+    )
+}
+
+export default Input
